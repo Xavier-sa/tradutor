18f92ac92572e38ef8bdad2dc4d779ca5f0b4919
W.Xavier <wellington222688@gmail.com>
2024-12-16 22:35:20 -0300

    creie a funcao carregar-dicionairo , que busca

--- model/Dicionario.py
+++ model/Dicionario.py
@@ -4,3 +4,13 @@ class DicionarioModel:
     def __init__(self, arquivo='dicionario.json'):
         self.arquivo = arquivo
         self.dicionario = self.carregar_dicionario()
+        
+    def carregar_dicionario(self):
+        #Carrega o dicionario do arquivo JSON
+        try:
+            with open(self.arquivo,'r',encoding='utf-8') as arquivo:
+                return json.load(arquivo)
+            
+        except FileNotFoundError:
+            return{}
+        
